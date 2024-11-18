4ab86f7f56ba02dcaf74bd91d81112ce81bcc913
Juan Pablo <jd301006@gmail.com>
2024-11-18 08:13:00 -0500

    b

--- science-project/src/routes/evaluarme/ex.estados/+page.svelte
+++ science-project/src/routes/evaluarme/ex.estados/+page.svelte
@@ -86,7 +86,7 @@
 <br>
   <h3>Puntuación: {puntuacion} / 100</h3>
   <br>
-  <a href="/evaluarme">Volver</a>
+  <a class="b" href="/evaluarme">Volver</a>
   <br>
 </main>
 <br>
@@ -152,5 +152,45 @@ margin-top: 20px;
 
 button:hover {
 background-color: #0056b3;
+}
+
+/* From Uiverse.io by alexroumi */ 
+.b {
+ padding: 15px 25px;
+ border: unset;
+ border-radius: 15px;
+ color: #212121;
+ z-index: 1;
+ background: #e8e8e8;
+ position: relative;
+ font-weight: 1000;
+ font-size: 17px;
+ -webkit-box-shadow: 4px 8px 19px -3px rgba(0,0,0,0.27);
+ box-shadow: 4px 8px 19px -3px rgba(0,0,0,0.27);
+ transition: all 250ms;
+ overflow: hidden;
+}
+
+.b::before {
+ content: "";
+ position: absolute;
+ top: 0;
+ left: 0;
+ height: 100%;
+ width: 0;
+ border-radius: 15px;
+ background-color: #212121;
+ z-index: -1;
+ -webkit-box-shadow: 4px 8px 19px -3px rgba(0,0,0,0.27);
+ box-shadow: 4px 8px 19px -3px rgba(0,0,0,0.27);
+ transition: all 250ms
+}
+
+.b:hover {
+ color: #e8e8e8;
+}
+
+.b:hover::before {
+ width: 100%;
 }
 </style>
